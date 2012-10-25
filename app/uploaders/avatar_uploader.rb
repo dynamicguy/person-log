96ca59bad10a84a70496c19e2d262c2196d5882b
Nurul Ferdous <nurul@ferdo.us>
2012-10-26 01:38:56 +0600

    search result page style fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -35,7 +35,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [200, 117]
+    process :resize_to_fit => [250, 117]
   end
 
   version :square do
